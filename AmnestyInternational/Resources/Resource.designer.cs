bbfd2459b9e39ce07e4adda818a641c09132b74e
glenn <glennvanhaute@gmail.com>
2015-12-29 21:11:20 +0100

    added amnesty logo

--- AmnestyInternational/Resources/Resource.designer.cs
+++ AmnestyInternational/Resources/Resource.designer.cs
@@ -41,71 +41,90 @@ namespace AmnestyInternational
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int sample1 = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050010
-			public const int autoCompleteTextView1 = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int autoCompleteTextView1 = 2131099664;
 			
-			// aapt resource value: 0x7f050006
-			public const int backToMain = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int backToMain = 2131099654;
 			
-			// aapt resource value: 0x7f050012
-			public const int button2 = 2131034130;
+			// aapt resource value: 0x7f060012
+			public const int button2 = 2131099666;
 			
-			// aapt resource value: 0x7f050002
-			public const int checkedTextView2 = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int checkedTextView2 = 2131099650;
 			
-			// aapt resource value: 0x7f050014
-			public const int donate = 2131034132;
+			// aapt resource value: 0x7f060014
+			public const int demoImageView = 2131099668;
 			
-			// aapt resource value: 0x7f050003
-			public const int editText1 = 2131034115;
+			// aapt resource value: 0x7f060015
+			public const int donate = 2131099669;
 			
-			// aapt resource value: 0x7f050008
-			public const int editText2 = 2131034120;
+			// aapt resource value: 0x7f060003
+			public const int editText1 = 2131099651;
 			
-			// aapt resource value: 0x7f05000a
-			public const int editText3 = 2131034122;
+			// aapt resource value: 0x7f060008
+			public const int editText2 = 2131099656;
 			
-			// aapt resource value: 0x7f05000c
-			public const int editText4 = 2131034124;
+			// aapt resource value: 0x7f06000a
+			public const int editText3 = 2131099658;
 			
-			// aapt resource value: 0x7f05000e
-			public const int editText5 = 2131034126;
+			// aapt resource value: 0x7f06000c
+			public const int editText4 = 2131099660;
 			
-			// aapt resource value: 0x7f050011
-			public const int goToBedrag = 2131034129;
+			// aapt resource value: 0x7f06000e
+			public const int editText5 = 2131099662;
 			
-			// aapt resource value: 0x7f050013
-			public const int linearLayout1 = 2131034131;
+			// aapt resource value: 0x7f060011
+			public const int goToBedrag = 2131099665;
 			
-			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			// aapt resource value: 0x7f060013
+			public const int linearLayout1 = 2131099667;
 			
-			// aapt resource value: 0x7f050001
-			public const int textView2 = 2131034113;
+			// aapt resource value: 0x7f060000
+			public const int textView1 = 2131099648;
 			
-			// aapt resource value: 0x7f050004
-			public const int textView3 = 2131034116;
+			// aapt resource value: 0x7f060001
+			public const int textView2 = 2131099649;
 			
-			// aapt resource value: 0x7f050007
-			public const int textView4 = 2131034119;
+			// aapt resource value: 0x7f060004
+			public const int textView3 = 2131099652;
 			
-			// aapt resource value: 0x7f050009
-			public const int textView5 = 2131034121;
+			// aapt resource value: 0x7f060007
+			public const int textView4 = 2131099655;
 			
-			// aapt resource value: 0x7f05000b
-			public const int textView6 = 2131034123;
+			// aapt resource value: 0x7f060009
+			public const int textView5 = 2131099657;
 			
-			// aapt resource value: 0x7f05000d
-			public const int textView7 = 2131034125;
+			// aapt resource value: 0x7f06000b
+			public const int textView6 = 2131099659;
 			
-			// aapt resource value: 0x7f05000f
-			public const int textView8 = 2131034127;
+			// aapt resource value: 0x7f06000d
+			public const int textView7 = 2131099661;
 			
-			// aapt resource value: 0x7f050005
-			public const int toggleButton1 = 2131034117;
+			// aapt resource value: 0x7f06000f
+			public const int textView8 = 2131099663;
+			
+			// aapt resource value: 0x7f060005
+			public const int toggleButton1 = 2131099653;
 			
 			static Id()
 			{
@@ -120,14 +139,14 @@ namespace AmnestyInternational
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Bedrag = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Bedrag = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int Contactgegevens = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int Contactgegevens = 2130968577;
 			
-			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int Main = 2130968578;
 			
 			static Layout()
 			{
@@ -142,8 +161,8 @@ namespace AmnestyInternational
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -158,11 +177,11 @@ namespace AmnestyInternational
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
