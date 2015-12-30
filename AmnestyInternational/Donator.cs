f7886718de8f7138ffe1052c91d98c8b33b00b22
Jasper De Smet <jasp.desmet@gmail.com>
2015-12-30 18:46:26 +0100

    Make object parsable

--- AmnestyInternational/Donator.cs
+++ AmnestyInternational/Donator.cs
@@ -2,7 +2,7 @@
 
 namespace AmnestyInternational
 {
-	public class Donator
+	public class Donator : Java.Lang.Object, IParcelable
 	{
 		public string name { get; set; }
 		public string email { get; set; }
@@ -15,7 +15,33 @@ namespace AmnestyInternational
 		// constructor
 		public Donator ()
 		{
+			
+		}
 
+		public Donator(Parcel parcel) {
+			name = parcel.ReadString ();
+		}
+
+		public int DescribeContents()
+		{
+			return 0;
+		}
+
+		// Save this instance's values to the parcel
+		public void WriteToParcel(Parcel dest, ParcelableWriteFlags flags)
+		{
+			dest.WriteInt(Id);
+			dest.WriteString(Name);
+		}
+
+		// The creator creates an instance of the specified object
+		private static readonly GenericParcelableCreator<SelectListItem> _creator
+		= new GenericParcelableCreator<SelectListItem>((parcel) => new SelectListItem(parcel));
+
+		[ExportField("CREATOR")]
+		public static GenericParcelableCreator<SelectListItem> GetCreator()
+		{
+			return _creator;
 		}
 	}
 }
